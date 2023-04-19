983c52e30671d000d106ea665c88b6c648fed734
bjornarsk <bjornar2107@live.no>
2023-04-19 12:50:42 +0200

    modified:   mycrypt/mycrypt.go

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,14 +1,12 @@
 package mycrypt
 
+import "unicode"
+
 var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
-		// Convert any uppercase letters to lowercase
-		if melding[i] >= 'A' && melding[i] <= 'Z' {
-			melding[i] = melding[i] - 'A' + 'a'
-		}
 		indeks := sokIAlfabetet(melding[i], alphabet)
 		if indeks+chiffer >= len(alphabet) {
 			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
@@ -20,13 +18,8 @@ func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 }
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
-	// Convert any uppercase letters to lowercase
-	if symbol >= 'A' && symbol <= 'Z' {
-		symbol = symbol - 'A' + 'a'
-	}
-
 	for i := 0; i < len(alfabet); i++ {
-		if symbol == alfabet[i] {
+		if unicode.ToLower(symbol) == unicode.ToLower(alfabet[i]) {
 			return i
 		}
 	}
