fb9940d2d4e8b955da3ed364b98bf2f272716d51
bjornarsk <bjornar2107@live.no>
2023-04-19 13:27:43 +0200

    jkb

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -7,10 +7,6 @@ var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; AB
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
-		if melding[i] == ' ' {
-			kryptertMelding[i] = rune(63)
-			continue
-		}
 		indeks := sokIAlfabetet(melding[i], alphabet)
 		if indeks+chiffer >= len(alphabet) {
 			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
@@ -28,21 +24,4 @@ func sokIAlfabetet(symbol rune, alfabet []rune) int {
 		}
 	}
 	return -1
-}
-
-func Dekrypter(melding []rune, alphabet []rune, chiffer int) []rune {
-	dekryptertMelding := make([]rune, len(melding))
-	for i := 0; i < len(melding); i++ {
-		if melding[i] == rune(63) {
-			dekryptertMelding[i] = ' '
-			continue
-		}
-		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks-chiffer < 0 {
-			dekryptertMelding[i] = alphabet[indeks-chiffer+len(alphabet)]
-		} else {
-			dekryptertMelding[i] = alphabet[indeks-chiffer]
-		}
-	}
-	return dekryptertMelding
 }
